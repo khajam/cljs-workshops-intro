0c3df0f3c7c8f16deefa7568129cb6b2fbe16202
den1k <den1k@me.com>
2015-03-25 17:48:23 -0400

    normalize coords on diameter to reach full color range

--- src/intro/core.cljs
+++ src/intro/core.cljs
@@ -14,6 +14,11 @@
   (fn [b]
     (f a b)))
 
+(defn normalize [min max]
+  (fn [x]
+    (* (/ 1 (- max min)) x)))
+
+((normalize 0 (* 2 80)) 160)
 ;; define your app data so that it doesn't get over-written on reload
 
 (defonce app-state (atom {:text "Hello world!"}))
@@ -24,21 +29,25 @@
     (display-name [_] "sun-view")
     om/IInitState
     (init-state [_]
-      {:r 200
-       :g 0
-       :b 0
-       :rel-x-fn nil})
+      (let [radius 80]
+        {:radius       radius
+         :normalize-fn (normalize 0 (* 2 radius))
+         :r            200
+         :g            0
+         :b            0
+         :rel-x-fn     identity}))
     om/IRenderState
-    (render-state [_ {:keys [r g b rel-x-fn]}]
+    (render-state [_ {:keys [radius normalize-fn r g b rel-x-fn]}]
       (dom/svg #js {:height 200 :width 200}
-               (dom/circle #js {:r            80 :cx 80 :cy 80
+               (dom/circle #js {:r            radius :cx 80 :cy 80
                                 :fill         (str "rgb(" (join "," [r g b]) ")")
                                 :onMouseEnter #(let [x (.-clientX %)]
-                                                (om/set-state! owner :rel-x-fn (with-init - x)))
+                                                (om/set-state! owner :rel-x-fn (with-init (comp Math/abs -) x)))
                                 :onMouseMove  #(let [x (.-clientX %)]
                                                 (l "x:" x)
                                                 (l "rel-x:" (rel-x-fn x))
-                                                (om/set-state! owner :r (Math/abs (rel-x-fn x))))})))))
+                                                (l "nomrd" (Math/round (* 255 (normalize-fn (rel-x-fn x)))))
+                                                (om/set-state! owner :r (Math/round (* 255 (normalize-fn (rel-x-fn x))))))})))))
 
 (om/root
   (fn [data owner]
@@ -51,6 +60,8 @@
                                   :display        "flex"
                                   :justifyContent "space-around"
                                   :alignItems     "center"}}
+                 (om/build sun-view data)
+                 (om/build sun-view data)
                  (om/build sun-view data)
                  (dom/h1 nil (:text data))))))
   app-state
